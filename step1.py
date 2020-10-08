b468811b528b7ea6c21c97056d1ad9e621e2c4e6
Nelson A. Morais <nelson.a.morais@inesctec.pt>
2020-10-08 15:19:24 +0100

    Revert "Reduced amount of scripts"
    This reverts commit 71822cdf24a9fc86761e5c23da0451bc4746c395.

---
+++ step1.py
@@ -0,0 +1,26 @@
+# Download data, unzip, etc.
+import pandas as pd
+import urllib.request  # Import 'urllib' when Python 2 is adopted
+import tempfile
+import shutil
+import zipfile
+
+temp_dir = tempfile.mkdtemp()
+data_source = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip'
+zipname = temp_dir + '/Bike-Sharing-Dataset.zip'
+urllib.request.urlretrieve(data_source, zipname)  # urllib.urlretrieve(data_source, zipname)
+
+zip_ref = zipfile.ZipFile(zipname, 'r')
+zip_ref.extractall(temp_dir)
+zip_ref.close()
+
+daily_path = temp_dir + '/day.csv'
+daily_data = pd.read_csv(daily_path)
+daily_data['dteday'] = pd.to_datetime(daily_data['dteday'])
+drop_list = ['instant', 'season', 'yr', 'mnth', 'holiday', 'workingday', 'weathersit', 'atemp', 'hum']
+daily_data.drop(drop_list, inplace=True, axis=1)
+
+shutil.rmtree(temp_dir)
+
+
+daily_data.head()
---
+++ step2.py
@@ -0,0 +1,20 @@
+from __future__ import division, print_function
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+# In a notebook environment, display the plots inline
+'exec(%matplotlib inline)'  # Adopted from IPython
+
+# Set some parameters to apply to all plots. These can be overridden
+# in each plot if desired
+import matplotlib
+# Plot size to 14" x 7"
+matplotlib.rc('figure', figsize = (14, 7))
+# Font size to 14
+matplotlib.rc('font', size = 14)
+# Do not display top and right frame lines
+matplotlib.rc('axes.spines', top = False, right = False)
+# Remove grid lines
+matplotlib.rc('axes', grid = False)
+# Set backgound color to white
+matplotlib.rc('axes', facecolor = 'white')
